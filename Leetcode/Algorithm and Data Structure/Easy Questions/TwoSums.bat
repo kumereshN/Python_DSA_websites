fb86293d2ed8d2ee6b998ae888dfb770e6d63d09
kumereshN <kumeresh6@hotmail.com.com>
2021-06-29 07:17:06 +0800

    push

---
+++ Leetcode/Algorithm and Data Structure/Easy Questions/TwoSums.bat
@@ -0,0 +1,2 @@
+@py.exe "C:\Users\Trogg\Documents\Python Scripts\Leetcode\Algorithm and Data Structure\Easy Questions\Two_Sums.py" %*
+@pause
--- Leetcode/Algorithm and Data Structure/Easy Questions/Two_Sums.py
+++ Leetcode/Algorithm and Data Structure/Easy Questions/Two_Sums.py
@@ -1,9 +1,12 @@
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {} # val : index
+def twoSum(nums, target):
+    prevMap = {} # val : index
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
-        return
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+
+nums = [3,2,4]
+target = 6
+print(twoSum(nums,target))
