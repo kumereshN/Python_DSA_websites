9b0873bf91fc1a7688258fd01412aaa4866626fb
kumereshN <kumeresh6@hotmail.com.com>
2021-10-19 08:08:57 +0800

    update

--- Leetcode/Algorithm and Data Structure/Medium Questions/longest_substr_without_repeating_char.py
+++ Leetcode/Algorithm and Data Structure/Medium Questions/longest_substr_without_repeating_char.py
@@ -2,17 +2,23 @@ def lengthOfLongestSubstring(s):
     n = len(s)
     ans = i = 0
     # mp stores the current index of a character, to keep track of the index of the characters
+    # Indexes are 1-based not starting from 0
     mp = {}
-    
-    # try to extend the range [i, j]
-    for j in range(n): # j is the pointer that moves until it reaches a character that is similar to s[j], which is s[j] == s[i].
-        if s[j] in mp: # If the character exists in the hash map, which is the repeating character.
-            i = max(mp[s[j]], i) # Compare the index of the repeating character in the hash map against the index i and set the maximum value for i.
 
-        ans = max(ans, j - i + 1) # Gets the maximum between ans and j - i + 1, which is the length of the string
+    # try to extend the range [i, j]
+    # j is the pointer that moves until it reaches a character that is similar to s[j], which is s[j] == s[i].
+    for j in range(n):
+        # If the character exists in the hash map, which is the repeating character.
+        if s[j] in mp:
+            # Compare the index of the repeating character in the hash map against the index i and set the maximum value for i.
+            i = max(mp[s[j]], i)
+
+        # Gets the maximum between ans and j - i + 1, which is the length of the string
+        ans = max(ans, j - i + 1)
         mp[s[j]] = j + 1
 
     return ans
+
 
 s = "abcabcbb"
 lengthOfLongestSubstring(s)
@@ -27,8 +33,8 @@ i is the index of the repeating character. It acts as an anchor (e.g:j-i+1) j co
 def lengthOfLongestSubstring(s):
     dicts = {}
     maxlength = start = 0
-    for i,value in enumerate(s):
-        if value in dicts: # If the character in the dictionary
+    for i, value in enumerate(s):
+        if value in dicts:  # If the character in the dictionary
             sums = dicts[value] + 1
             if sums > start:
                 start = sums
