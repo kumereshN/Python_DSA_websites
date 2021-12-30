fa5e9f27fec6e56deeb38ff69a9dd41def853e1f
kumereshN <kumeresh6@hotmail.com.com>
2021-12-30 22:56:14 +0800

    updates

--- AlgoMonster/Dedup/combination_sum.py
+++ AlgoMonster/Dedup/combination_sum.py
@@ -2,17 +2,22 @@ from typing import List
 
 
 def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
-    def dfs(start_index, nums, remaining, path):
+    # WRITE YOUR BRILLIANT CODE HERE
+    res = []
+    n = len(candidates)
+
+    def dfs(start, nums, remaining, path):
         if remaining == 0:
             res.append(path[:])
             return
         # start_index is to keep track of the index moving, if not it'll reset to 0 everytime it does dfs
-        for i in range(start_index, len(nums)):
+        for i in range(start, n):
             num = nums[i]
             # If remaining - num is less than 0, we prune the branch
+            # if the remaining - num is less than 0, we move to the next number in the list
             if remaining - num < 0:
                 continue
-            dfs(nums, i, remaining - num, path + [num])
-    res = []
-    dfs(0, candidates, target, [])
-    return res
+            # Carry i to the next recursion
+            dfs(i, nums, remaining - num, path + [num])
+        return res
+    return dfs(0, candidates, target, [])
