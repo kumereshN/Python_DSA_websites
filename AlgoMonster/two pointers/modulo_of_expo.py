0d32abc0afa2da65612e809a8192e11946cc80b3
kumereshN <kumeresh6@hotmail.com.com>
2022-01-16 17:30:24 +0800

    updates

--- AlgoMonster/two pointers/modulo_of_expo.py
+++ AlgoMonster/two pointers/modulo_of_expo.py
@@ -2,6 +2,7 @@ def step(n, m):
     return (n ** 2) % m
 
 def modulo_of_exponent(n, k, m):
+    # First part
     k -= 1
     tortoise = step(n, m)
     hare = step(step(n, m), m)
@@ -11,6 +12,7 @@ def modulo_of_exponent(n, k, m):
         hare = step(step(hare, m), m)
     if k == 0:
         return tortoise
+    # Second part
     cycle_size = 1
     tortoise = step(tortoise, m)
     hare = step(step(hare, m), m)
@@ -22,6 +24,7 @@ def modulo_of_exponent(n, k, m):
         cycle_size += 1
     if k == 0:
         return tortoise
+    # Third part
     k = k % cycle_size
     while k > 0:
         tortoise = step(tortoise, m)
