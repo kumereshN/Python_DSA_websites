f0c8cdacf3343f14e191eaa450855c84ad54693d
kumereshN <kumeresh6@hotmail.com>
2022-02-18 15:58:59 +0800

    updates to no_island_with_visited.py

--- AlgoMonster/graphs/matrix as graphs/no_islands_with_visited.py
+++ AlgoMonster/graphs/matrix as graphs/no_islands_with_visited.py
@@ -4,7 +4,7 @@ from collections import deque
 def count_number_of_islands(grid: List[List[int]]) -> int:
     # WRITE YOUR BRILLIANT CODE HERE
     num_row, num_col = len(grid), len(grid[0])
-    visited = [[False for _ in range(num_col)] for _ in range(num_row)]
+    
     
     
     def get_neighbor(coord):
@@ -18,27 +18,28 @@ def count_number_of_islands(grid: List[List[int]]) -> int:
             if 0 <= neighbor_row < num_row and 0 <= neighbor_col < num_col:
                 yield neighbor_row, neighbor_col
                 
-    def bfs(root):
+    def bfs(root, visited):
         row, col = root
         queue = deque([root])
         visited[row][col] = True
-        
+
         while len(queue) > 0:
             node = queue.popleft()
             for neighbor in get_neighbor(node):
                 row, col = neighbor
-                if visited[row][col] == True:
+                if grid[row][col] == 0 or visited[row][col]:
                     continue
                 visited[row][col] = True
                 queue.append(neighbor)
     
     count = 0
-    
+    visited = [[False for _ in range(num_col)] for _ in range(num_row)]
+
     for row in range(num_row):
         for col in range(num_col):
-            if visited[row][col]:
+            if grid[row][col] == 0 or visited[row][col]:
                 continue
-            bfs((row, col))
+            bfs((row, col), visited)
             count += 1
     return count
 
