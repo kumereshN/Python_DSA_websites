229cc4ac197d43583618a0bb2404950a09590ca0
kumereshN <kumeresh6@hotmail.com.com>
2022-03-04 18:23:49 +0800

    updates

---
+++ AlgoMonster/Binary Search/newspapers.py
@@ -0,0 +1,32 @@
+from typing import List
+
+def feasible(newspapers: List[int], max_time: int, coworkers: int) -> bool:
+    """ Same as capacity to ship packages problem """
+    i = 0
+    n = len(newspapers)
+    total_time = max_time
+    req_coworkers = 1
+    
+    while i < n:
+        if newspapers[i] <= total_time:
+            total_time -= newspapers[i]
+            i += 1
+        else:
+            total_time = max_time
+            req_coworkers += 1
+    return req_coworkers <= coworkers
+
+def newspapers_split(newspapers: List[int], coworkers: int) -> int:
+    # WRITE YOUR BRILLIANT CODE HERE
+    min_ptr = max(newspapers)
+    max_ptr = sum(newspapers)
+    boundary_index = max_ptr
+    
+    while min_ptr <= max_ptr:
+        mid = (min_ptr + max_ptr)//2
+        if feasible(coworkers, mid, newspapers):
+            max_ptr = mid - 1
+            boundary_index = mid
+        else:
+            min_ptr = mid + 1
+    return boundary_index
