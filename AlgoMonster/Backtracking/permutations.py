267e97c6813f235db95c2d7bf3ebb5bcc2bd691e
kumereshN <kumeresh6@hotmail.com.com>
2022-03-13 18:28:54 +0800

    updtes to permutations.py

--- AlgoMonster/Backtracking/permutations.py
+++ AlgoMonster/Backtracking/permutations.py
@@ -1,29 +1,63 @@
-def permutations(l):
-    # WRITE YOUR BRILLIANT CODE HERE
-    if not l:
-        return []
-    res = []
-    permutation = []
+"""
+User Solution
+"""
+# def permutations(l):
+#     # WRITE YOUR BRILLIANT CODE HERE
+#     if not l:
+#         return []
+#     res = []
+#     permutation = []
 
-    def dfs(l, permutation):
-        # Base case
-        if len(permutation) == len(l):
-            # We're making a copy of the list to be appended to res as the list will be changed in subsequent recursions
-            res.append(list(permutation))
-            # Exit out of the recursion
+#     def dfs(l, permutation):
+#         # Base case
+#         if len(permutation) == len(l):
+#             # We're making a copy of the list to be appended to res as the list will be changed in subsequent recursions
+#             res.append(list(permutation))
+#             # Exit out of the recursion
+#             return
+
+#         for letter in l:
+#             if letter not in permutation:
+#                 permutation.append(letter)
+#                 dfs(l, permutation)
+#                 # Backtracking
+#                 permutation.pop()
+
+#     dfs(l, permutation)
+#     return res
+
+
+# if __name__ == "__main__":
+#     res = permutations(list(input()))
+#     print(' '.join(sorted(''.join(x) for x in res)))
+
+"""
+Algo monster solution
+"""
+
+from typing import List
+
+def permutations(letters):
+    def dfs(path, used, res):
+        if len(path) == len(letters):
+            res.append(''.join(path))
             return
 
-        for letter in l:
-            if letter not in permutation:
-                permutation.append(letter)
-                dfs(l, permutation)
-                # Backtracking
-                permutation.pop()
+        for i, letter in enumerate(letters):
+            # skip used letters
+            if used[i]:
+                continue
+            # add letter to permutation, mark letter as used
+            path.append(letter)
+            used[i] = True
+            dfs(path, used, res)
+            # remove letter from permutation, mark letter as unused
+            path.pop()
+            used[i] = False
 
-    dfs(l, permutation)
+    res = []
+    dfs([], [False] * len(letters), res)
     return res
 
-
-if __name__ == "__main__":
-    res = permutations(list(input()))
-    print(' '.join(sorted(''.join(x) for x in res)))
+letters = 'abc'
+print(permutations(letters))
