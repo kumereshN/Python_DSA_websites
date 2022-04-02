33d74835368526c8df858eb59935b3e279c71097
kumereshN <kumeresh6@hotmail.com.com>
2022-04-02 17:39:00 +0800

    updates

--- AlgoMonster/sliding windows/longest_substring_without_repeating_chars.py
+++ AlgoMonster/sliding windows/longest_substring_without_repeating_chars.py
@@ -6,7 +6,7 @@ def longest_substring_without_repeating_characters(s: str) -> int:
     
     for right, ch in enumerate(s):
         if ch in seen:
-            # we want to get the max position of the left pointer
+            # if we've seen the char previously, we move past the last seen char position and get the next char's position, hence seen[ch] + 1
             left = max(left, seen[ch]+1)
         res = max(res, right - left + 1)
         seen[ch] = right
