6d7a9a77f8ae3ebab24c284c32b42686ab9a689d
kumereshN <kumeresh6@hotmail.com.com>
2022-04-05 08:01:04 +0800

    updates

--- AlgoMonster/sliding windows/longest_substring_without_repeating_chars.py
+++ AlgoMonster/sliding windows/longest_substring_without_repeating_chars.py
@@ -7,6 +7,7 @@ def longest_substring_without_repeating_characters(s: str) -> int:
     for right, ch in enumerate(s):
         if ch in seen:
             # if we've seen the char previously, we move past the last seen char position and get the next char's position, hence seen[ch] + 1
+            # if the seen[ch]+1 index is larger than the last known left index, replace it with seen[ch]+1.
             left = max(left, seen[ch]+1)
         res = max(res, right - left + 1)
         seen[ch] = right
