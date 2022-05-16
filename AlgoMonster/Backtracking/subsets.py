3518ee8a752d6d9914d64a16a39d775b2102e783
kumereshN <kumeresh6@hotmail.com.com>
2022-05-16 11:12:20 +0800

    updates to subsets.py

--- AlgoMonster/Backtracking/subsets.py
+++ AlgoMonster/Backtracking/subsets.py
@@ -1,16 +1,17 @@
 def subsets(nums):
     # WRITE YOUR BRILLIANT CODE HERE
-    res = []
+    res = [[]]
 
     # The path carries the state to the next recursion
     def dfs(i, path):
         # Base case: if the index is the len(nums), append the path to result
         # it goes back to the previous stack, e.g: i = 3 then falls back to 2
         if i == len(nums):
-            res.append(path)
             return
         # DFS as normal
-        dfs(i+1, path + [nums[i]])
+        num = nums[i]
+        res.append(path + [num])
+        dfs(i+1, path + [num])
         # this backtracks to the previous node, and checks for other combinations from the nums
         # Example [1,2] and [1,3]
         dfs(i+1, path)
@@ -19,4 +20,4 @@ def subsets(nums):
 
 
 nums = [1, 2, 3]
-subsets(nums)
+print(subsets(nums))
